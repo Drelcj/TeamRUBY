cd171f504e29b65bccabe5d707ec4c9986bd9180
Dukesanmi <42076983+Dukesanmi@users.noreply.github.com>
2019-09-17 04:15:16 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php require_once 'auth/sess.php'; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head></head>
+
+<body>
+<h3>TeamRuby</h3>
+<p>Welcome to our world!</p>
+<p><a href="auth/logout.php">Logout</a>
+
+
+</body>
+
+</html>
